eb9f8713f8fede1e6d2f7d9c400416aee7f27a93
EliasAHH <eliasiscoding@gmail.com>
2021-03-01 15:48:29 -0800

    lecture done

--- index.js
+++ index.js
@@ -14,22 +14,25 @@ function init() {
 
 function handleSubmit(e) {
     e.preventDefault();
-    // console.log(e)
-    // const post = {
-    //     title: e.target[0].value,
-    //     content: e.target[1].value,
-    //     author: e.target[2].value
-    // }
-    // fetch(baseUrl, { 
-    //     method: "POST",
-    //     headers: {
-    //         'Content-Type': 'application/json'
-    //     },
-    //     body: post
+    // let title = document.getElementById("blog-title").value;
+    // let author = document.getElementById("blog-author").value;
+    // let content = document.getElementById("blog-content").value;
+    const postInfo = {
+        title: e.target[0].value,
+        author: e.target[1].value,
+        content: e.target[2].value
+    }
 
-    // })
-    //     .then(r => r.json())
-    //     .then(data => putPostOnDom(data))
+    // by default Fetch makes a GET request, if you want to make any other request you HAVE to tell it what kind
+    fetch(baseUrl, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(postInfo)
+    })
+        .then(r => r.json())
+        .then(data => putPostOnDom(data))
 }
 
 function getAllPosts() {
@@ -40,30 +43,83 @@ function getAllPosts() {
 }
 
 function handlePosts(postsArr) {
-    postsArr.forEach(p => putPostOnDom(p));
+    postsArr.forEach(p => {
+        putPostOnDom(p)
+    });
+  // You only do this IF and only IF  you're doing the innerHTML  += because you have to look through all the div's and give each button an event listener. 
+    // let likebutton = Array.from(document.getElementsByClassName("blog-posts"));
+    // likebutton.forEach(div => {
+    //     let button = div.querySelector("button")
+    //     button.addEventListener("click",handleLikes)
+    // })
+
 }
 
 
 function putPostOnDom(post) {
-    // let div = document.createElement("div")
-    // let li = document.createElement("li")
-    // let p1 = document.createElement("p")
-    // let p2 = document.createElement("p")
-    // let p3 = document.createElement("P")
-    // p1.innerText = post.title
-    // p2.innerText = post.content
-    // p3.innerText = post.author
 
-    // li.append(p1, p2, p3)
-    // div.append(li)
-    // ul.append(div)
-    ul.innerHTML += `
-        <div id=${post.id} class="blog-posts"> 
-            <li>
-                <h1> Title: ${post.title} </h1>
-                <h2> Content: ${post.content} </h2>  
-                <h5> Author: ${post.author.name} </h5> 
-             </li> 
-        </div> 
-    `
+
+    let div = document.createElement("div")
+    let li = document.createElement("li")
+    let p1 = document.createElement("p")
+    let p2 = document.createElement("p")
+    let p3 = document.createElement("p")
+    let button = document.createElement("button")
+    let span = document.createElement("span")
+
+
+    p1.innerText = post.title
+    p2.innerText = post.content
+    p3.innerText = post.author.name
+    button.innerText = "Likes"
+    button.id = post.id
+    span.innerText = post.likes
+    button.append(span)
+
+
+    li.append(p1, p2, p3, button)
+    div.append(li)
+    ul.append(div)
+
+    // here you can give each seperate button it's own event listener without having to query on it.
+    button.addEventListener("click", handleLikes)
+
+
+  // if you do it this way you must iterate through all the div's and give each button an event listener
+//     ul.innerHTML += `
+//         <div class="blog-posts"> 
+//             <li>
+//                 <h1> Title: ${post.title} </h1>
+//                 <h2> Content: ${post.content} </h2>  
+//                 <h5> Author: ${post.author.name} </h5>  
+//                 <button id=${post.id}> Likes <span> ${post.likes} </span> <button> 
+//              </li> 
+//         </div> 
+//   `
+    }
+
+
+    // optimistic rendering - Which means we Update the DOM BEFORE we make any changes to our database. 
+    // pesimistic rendering - Which means we update the database BEFORE we make any changes to our DOM
+
+function handleLikes(e) {
+  
+    let likes = parseInt(e.target.children[0].innerText)
+    fetch(baseUrl + `/${e.target.id}`, {
+        method: "PATCH",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({likes: likes += 1 })
+    })
+
+        .then(r => r.json())
+        .then(data => updateDom(data,e.target.id))
 }
+
+
+function updateDom(data,id) {
+    let button = document.getElementById(`${id}`)
+    button.children[0].innerText = data.likes
+}
+
